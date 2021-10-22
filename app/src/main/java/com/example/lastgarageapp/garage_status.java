63e7b6824f5ac8b0623520671518f018a4265482
GarageApp <noorbarham@gmail.com>
2021-10-22 17:00:34 +0300

    our_Project

---
+++ app/src/main/java/com/example/lastgarageapp/garage_status.java
@@ -0,0 +1,14 @@
+package com.example.lastgarageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class garage_status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garage_status);
+    }
+}
---
+++ app/src/main/java/com/example/lastgarageapp/read_complaints.java
@@ -0,0 +1,14 @@
+package com.example.lastgarageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class read_complaints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_complaints);
+    }
+}
