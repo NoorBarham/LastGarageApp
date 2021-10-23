6f02d54b95b16e503f22532ceb8ee8948864594b
GarageApp <noorbarham@gmail.com>
2021-10-23 16:47:35 +0300

    last_edit2

--- app/src/main/java/com/example/lastgarageapp/home_page.java
+++ app/src/main/java/com/example/lastgarageapp/home_page.java
@@ -2,13 +2,31 @@ package com.example.lastgarageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home_page extends AppCompatActivity {
+
+    Button newsButt, statusButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        newsButt=findViewById(R.id.homePage_newsButt);
+        statusButt=findViewById(R.id.homePage_statusButt);
+
+        newsButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newsButt.setBackgroundColor(0xFF000000);
+                newsButt.setTextColor(0xFFFF6F00);
+                Intent intent= new Intent(home_page.this ,news.class);
+                startActivity(intent);
+            }
+        });
     }
 }
