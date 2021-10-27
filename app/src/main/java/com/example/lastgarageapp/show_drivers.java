5a9b91181daf40d9891abf9cbaf10ddffae58536
Sireen <sireenismail99@gmail.com>
2021-10-27 23:58:50 +0300

    edit

---
+++ app/src/main/java/com/example/lastgarageapp/show_drivers.java
@@ -0,0 +1,14 @@
+package com.example.lastgarageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class show_drivers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_drivers);
+    }
+}
