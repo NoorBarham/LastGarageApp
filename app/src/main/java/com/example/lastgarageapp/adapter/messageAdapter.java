f26b290153552eae07cdf36367e7c86b51aa1f28
GarageApp <noorbarham@gmail.com>
2021-10-29 19:12:27 +0300

    DoneMessageAdapter

--- app/src/main/java/com/example/lastgarageapp/adapter/messageAdapter.java
+++ app/src/main/java/com/example/lastgarageapp/adapter/messageAdapter.java
@@ -19,14 +19,12 @@ import java.util.List;
 public class messageAdapter extends RecyclerView.Adapter<messageAdapter.myViewHolder> {
 
     private List<String> mMessagesTexts ;
-    private ArrayList<String> mHoureTexts = new ArrayList<>();
-    private ArrayList<String> mAM_PM_Texts = new ArrayList<>();
+    private ArrayList<String> mHoureTexts;
     private LayoutInflater mInflater;
 
-    public messageAdapter(Context context,ArrayList<String> mMessagesTexts, ArrayList<String> mHoureTexts, ArrayList<String> mAM_PM_Texts) {
+    public messageAdapter(Context context,ArrayList<String> mMessagesTexts, ArrayList<String> mHoureTexts) {
         this.mMessagesTexts = mMessagesTexts;
         this.mHoureTexts = mHoureTexts;
-        this.mAM_PM_Texts = mAM_PM_Texts;
         this.mInflater = LayoutInflater.from(context);
     }
 
@@ -35,18 +33,24 @@ public class messageAdapter extends RecyclerView.Adapter<messageAdapter.myViewHo
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view= mInflater.inflate(R.layout.send_massage_item,parent,false);
         myViewHolder holder =new myViewHolder(view);
-        return holder;    }
+
+        return holder;
+    }
 
     @Override
     public void onBindViewHolder(myViewHolder holder, int position) {
         String message = mMessagesTexts.get(position);
         holder.sendMessageText.setText(message);
 
+        holder.sendMessageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // add action here for press on the text message
+            }
+        });
         String houre = mHoureTexts.get(position);
         holder.sendHoureText.setText(houre);
 
-        String am_pm = mAM_PM_Texts.get(position);
-        holder.send_am_pmText.setText(am_pm);
     }
 
     @Override
@@ -65,12 +69,18 @@ public class messageAdapter extends RecyclerView.Adapter<messageAdapter.myViewHo
             super(itemView);
             sendMessageText=itemView.findViewById(R.id.sendMessageItem_messageText);
             sendHoureText=itemView.findViewById(R.id.sendMessageItem_houre);
-            send_am_pmText=itemView.findViewById(R.id.sendMessageItem_AMPM);
             sendMessageLayout=itemView.findViewById(R.id.send_message_layout);
 //            receivedMessageText=itemView.findViewById(R.id.receivedMessageItem_messageText);
 //            receivedHoureText=itemView.findViewById(R.id.receivedMessageItem_houre);
 //            received_am_pmText=itemView.findViewById(R.id.receivedMessageItem_AMPM);
 //            receiveMessageLayout=itemView.findViewById(R.id.received_message_layout);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // add action here for press on the item
+                }
+            });
         }
+
     }
 }
--- app/src/main/java/com/example/lastgarageapp/conversation.java
+++ app/src/main/java/com/example/lastgarageapp/conversation.java
@@ -6,49 +6,63 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.lastgarageapp.adapter.messageAdapter;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class conversation extends AppCompatActivity {
 
-//    private ArrayList<String> MessagesTexts = new ArrayList<>();
-//    private ArrayList<String> HoureTexts = new ArrayList<>();
-//    private ArrayList<String> AM_PM_Texts = new ArrayList<>();
+    private ArrayList<String> MessagesTexts = new ArrayList<>();
+    private ArrayList<String> HoureTexts = new ArrayList<>();
 
+    ImageView conversation_send_icon;
+    TextView conversation_addText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
-        ArrayList<String> animalNames = new ArrayList<>();
-        animalNames.add("Horse");
-        animalNames.add("Cow");
-        animalNames.add("Camel");
-        animalNames.add("Sheep");
-        animalNames.add("Goat");
+        conversation_addText=findViewById(R.id.conversation_addText);
+        conversation_send_icon = findViewById(R.id.conversation_send_icon);
+        conversation_send_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final StringBuilder sb = new StringBuilder(conversation_addText.getText().length());
+                sb.append(conversation_addText.getText());
+                String s= sb.toString();
 
-        ArrayList<String> animalNames2 = new ArrayList<>();
-        animalNames.add("Horse");
-        animalNames.add("Cow");
-        animalNames.add("Camel");
-        animalNames.add("Sheep");
-        animalNames.add("Goat");
+                Date currentTime = Calendar.getInstance().getTime();
 
-        ArrayList<String> animalNames3 = new ArrayList<>();
-        animalNames.add("Horse");
-        animalNames.add("Cow");
-        animalNames.add("Camel");
-        animalNames.add("Sheep");
-        animalNames.add("Goat");
 
-        messageAdapter adapter;
+                MessagesTexts.add(s);
+                HoureTexts.add(currentTime.toString());
 
-        RecyclerView myRecyclerView = findViewById(R.id.converRecyView);
-        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new messageAdapter(this,animalNames,animalNames2, animalNames3);
-        myRecyclerView.setAdapter(adapter);
+                messageAdapter adapter;
+                RecyclerView myRecyclerView = findViewById(R.id.converRecyView);
+                myRecyclerView.setLayoutManager(new LinearLayoutManager(conversation.this));
+                adapter = new messageAdapter(conversation.this,MessagesTexts,HoureTexts);
+                myRecyclerView.setAdapter(adapter);
+                conversation_addText.setText("");
+            }
+        });
+
+//        MessagesTexts.add("مرحبا");
+//        HoureTexts.add("12:12");
+//        AM_PM_Texts.add("AM");
+//
+//        messageAdapter adapter;
+//
+//        RecyclerView myRecyclerView = findViewById(R.id.converRecyView);
+//        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        adapter = new messageAdapter(this,MessagesTexts,HoureTexts, AM_PM_Texts);
+//        myRecyclerView.setAdapter(adapter);
     }
 
 
