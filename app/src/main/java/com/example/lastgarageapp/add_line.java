2ecd1edc21dea5a94603f2216e6aa1e516aef414
GarageApp <noorbarham@gmail.com>
2021-11-25 00:55:18 +0200

    add_Line Spinner Done 🎉🎉🎉

--- app/src/main/java/com/example/lastgarageapp/add_line.java
+++ app/src/main/java/com/example/lastgarageapp/add_line.java
@@ -2,22 +2,24 @@ package com.example.lastgarageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.example.lastgarageapp.itemClasses.garageItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,7 +30,7 @@ public class add_line extends AppCompatActivity {
     private Button cancleButton,addButton;
     Spinner sour, des;
     EditText fare;
-    ArrayList city_arr;
+    ArrayList garage_arr =new ArrayList();
 
 
     @Override
@@ -39,31 +41,43 @@ public class add_line extends AppCompatActivity {
         sour = findViewById(R.id.addLine_sour);
         des = findViewById(R.id.addLine_dest);
         fare = findViewById(R.id.addLine_fare);
-
-//        city_arr.add("جنين");
-//        city_arr.add("طوباس");
-//        city_arr.add("نابلس");
-//        city_arr.add("طولكرم");
-//        city_arr.add("قلقيلية");
-//        city_arr.add("اريحا");
-//        city_arr.add("سلفيت");
-//        city_arr.add("رام الله");
-//        city_arr.add("البيرة");
-//        city_arr.add("القدس");
-//        city_arr.add("بيت لحم");
-//        city_arr.add("الخليل");
-//
-//        ArrayAdapter adapter1 = new ArrayAdapter(getBaseContext(), android.R.layout.simple_list_item_1,city_arr);
-//        sour.setAdapter(adapter1);
-
         addButton= findViewById(R.id.addline_addButt);
         cancleButton = findViewById(R.id.addLine_cancle);
 
+        //default
+        spinnerGarage();
+
+        sour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),adapterView.getItemAtPosition(i).toString() , Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        des.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),adapterView.getItemAtPosition(i).toString() , Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                if(fare.getText().length()==0||!sour.isSelected()||!des.isSelected()){
-                if(false){
+                if(fare.getText().length()==0){
                     Toast.makeText(getBaseContext(), "قم بإدخال جميع البيانات", Toast.LENGTH_SHORT).show();
                 }else{
                     String url = url_serverName.serverName+"addLine.php";
@@ -105,5 +119,39 @@ public class add_line extends AppCompatActivity {
     public String getData(EditText t){
         String myString =t.getText().toString();
         return myString;
+    }
+    public void spinnerGarage() {
+        String url = url_serverName.serverName + "spinnerGarage.php";
+        StringRequest myStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject object = new JSONObject(response);
+                    JSONArray jsonArray = object.getJSONArray("garages");
+                    garageItem myItem;
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject reader = jsonArray.getJSONObject(i);
+
+                        //String cityName
+                        String cityName = reader.getString("garage_name");
+                        garage_arr.add(cityName);
+
+                    }
+                    ArrayAdapter<CharSequence> adapter = new ArrayAdapter(add_line.this,android.R.layout.simple_spinner_item, garage_arr);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    des.setAdapter(adapter);
+                    sour.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), error.getMessage() + "", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        });
+        my_singleton.getInstance(add_line.this).addToRequestQueue(myStringRequest);
     }
 }
