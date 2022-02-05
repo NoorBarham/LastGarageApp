cd1ee29b46a469b29e73c48899e381b88a0dbf55
waeds <salmanwaed1@gmail.com>
2022-02-05 12:08:47 +0200

    .,ij

--- app/src/main/java/com/example/lastgarageapp/about.java
+++ app/src/main/java/com/example/lastgarageapp/about.java
@@ -9,7 +9,7 @@ public class about extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
+        setContentView(R.layout.activity_about);
 
     }
 }
